ab3b69393dcd1da867b44fe3b0820f60367af112
Georges987 <ayenigeorgepierre@gmail.com>
2023-02-02 00:24:52 +0100

    Adding code for ruby and python palyndrome

--- ruby/palyndrome.py
+++ ruby/palyndrome.py
@@ -1,2 +1,14 @@
-word = input("Entrez un mot :")
+def reversew(str):
+    reverse = ""
+    l = len(str)
+    i = l
+    while i !=0:
+        reverse += str[i-1]
+        i-=1
+    return reverse
 
+word = input("Entrez un mot :")
+if( word == reversew(word)):
+    print(word, "is a palyndrome")
+else:
+    print(word, "isn't a palyndrome")
--- ruby/palyndrome.rb
+++ ruby/palyndrome.rb
@@ -1,15 +1,8 @@
-word = ""
-while word != "q"
-  puts "Enter word"
-  word = gets.chomp
-  #system("cls || clear")
-  if word != "q" && word != "h"
-    if word == word.reverse
-      puts "Le mot #{word} est un palyndrome"
-    else
-      puts "Le mot #{word} n'est pas un palyndrome"
-    end
-  elsif word == "h"
-    puts "Usage: enter word and press enter \nq : for quit \nh : for help"
-  end
+puts "Enter word"
+word = gets.chomp
+system("cls || clear")
+if word == word.reverse
+  puts "Le mot #{word} est un palyndrome"
+else
+  puts "Le mot #{word} n'est pas un palyndrome"
 end
